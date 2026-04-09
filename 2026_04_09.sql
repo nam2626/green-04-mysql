3da9a2bd677a690123cceaf5ce65ec18d4b761e8
NamSangKyu <nam2626@naver.com>
2026-04-09 12:19:35 +0900

    Add SQL script for creating and managing students table in student_db

--- 2026_04_09.sql
+++ 2026_04_09.sql
@@ -9,4 +9,20 @@ create database student_db
 use student_db;
 
 -- 학생 테이블 생성
+create table students(
+	student_id int auto_increment primary key,
+    name varchar(50) not null,
+    grade int,
+    email varchar(200),
+    enrolled_at datetime default now()
+);
+-- 데이터베이스 목록 확인
+show databases;
+
+-- 테이블 삭제
+drop table students;
+-- DB 삭제
+drop database student_db;
+
+
 
