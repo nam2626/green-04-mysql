4b7b6dd31a785eb700b0133ec9105e234ac7f30f
NamSangKyu <nam2626@naver.com>
2026-04-17 17:13:23 +0900

    feat: Add CTEs for retrieving posts with like and hate counts and create board_view

--- 2026_04_17/board.sql
+++ 2026_04_17/board.sql
@@ -159,3 +159,34 @@ from board_like bl group by bl.bno) bl on bl.bno = b.bno
 join (select bh.bno, count(*) as bhate
 from board_hate bh group by bh.bno) bh on bh.bno = b.bno
 order by b.bno desc;
+
+-- WITH 절을 이용한 전체 게시글 조회 : MYSQL 8.0 이상에서 지원
+with board_content_like as 
+(select bl.bno, count(*) as blike from board_like bl group by bl.bno),
+board_content_hate as 
+(select bh.bno, count(*) as bhate from board_hate bh group by bh.bno)
+SELECT
+    b.bno, b.title, b.write_date,
+    b.content,b.bcount, bm.no, bm.nickname, 
+    ifnull(bcl.blike,0) as blike, ifnull(bch.bhate,0) as bhate
+from board b left outer join board_member bm on b.mno = bm.no
+LEFT outer join board_content_like bcl on bcl.bno = b.bno
+LEFT outer join board_content_hate bch on bch.bno = b.bno
+;
+
+CREATE OR REPLACE VIEW board_view
+as
+with board_content_like as 
+(select bl.bno, count(*) as blike from board_like bl group by bl.bno),
+board_content_hate as 
+(select bh.bno, count(*) as bhate from board_hate bh group by bh.bno)
+SELECT
+    b.bno, b.title, b.write_date,
+    b.content,b.bcount, bm.no, bm.nickname, 
+    ifnull(bcl.blike,0) as blike, ifnull(bch.bhate,0) as bhate
+from board b left outer join board_member bm on b.mno = bm.no
+LEFT outer join board_content_like bcl on bcl.bno = b.bno
+LEFT outer join board_content_hate bch on bch.bno = b.bno
+;
+
+select * from board_view;
